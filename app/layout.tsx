ad0b8fd24bfb84006020a0b548763b7080bcff26
thee28 <teetat2810@gmail.com>
2026-05-16 12:07:00 -0400

    Fixed css parsing error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      </head>
       <body className="min-h-full flex flex-col bg-[#EEF2FF]">
         <SessionProvider>{children}</SessionProvider>
       </body>
